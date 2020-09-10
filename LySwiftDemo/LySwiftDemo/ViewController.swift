3d90a2fe30ea25d2af8e0c02b02e3f16c629f595
samzjzhang <samzjzhang@tencent.com>
2020-09-10 11:11:39 +0800

    dev: 提交1

--- LySwiftDemo/LySwiftDemo/ViewController.swift
+++ LySwiftDemo/LySwiftDemo/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("dev - 1")
+        
     }
 
     override func didReceiveMemoryWarning() {
